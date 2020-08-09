e7fc36dc9f701b377aaa92c2c09973c4d6f72fb1
Tim Čas <darkuranium@gmail.com>
2020-08-09 21:21:39 +0200

    Add `coy_[v]aprintf()` to print to a newly-allocated string.

--- src/util/string.c
+++ src/util/string.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 char* coy_strdup_(const char* str, int len)
 {
@@ -10,4 +11,34 @@ char* coy_strdup_(const char* str, int len)
     memcpy(dup, str, len);  // we *cannot* just do `len+1`, because there might not be a trailing \0
     dup[len] = 0;
     return dup;
+}
+
+char* coy_vaprintf_(const char* format, va_list args)
+{
+    va_list argcpy;
+    va_copy(argcpy, args);
+#ifdef _WIN32   // Windows' vsnprintf() is non-compliant, so we need this
+    int len = _vscprintf(format, argcpy);
+#else
+    int len = vsnprintf(NULL, 0, format, argcpy);
+#endif
+    va_end(argcpy);
+    if(len < 0)
+        return NULL;
+    char* ret = malloc(len + 1);
+    len = vsnprintf(ret, len + 1, format, args);
+    if(len < 0)
+    {
+        free(ret);
+        return NULL;
+    }
+    return ret;
+}
+char* coy_aprintf_(const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    char* ret = coy_vaprintf_(format, args);
+    va_end(args);
+    return ret;
 }
--- src/util/string.h
+++ src/util/string.h
@@ -1,6 +1,11 @@
 #ifndef COY_UTIL_STRING_H_
 #define COY_UTIL_STRING_H_
 
+#include <stdarg.h>
+
 char* coy_strdup_(const char* str, int len);
+
+char* coy_vaprintf_(const char* format, va_list args);
+char* coy_aprintf_(const char* format, ...);
 
 #endif /* COY_UTIL_STRING_H_ */
