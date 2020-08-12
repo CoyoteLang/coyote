17ddb459a5ad094bb2f10276bd265fdb3d41c197
Tim Čas <darkuranium@gmail.com>
2020-08-12 15:37:45 +0000

    Add some comments in compatibility shims.

--- src/vm/compat_shims.h
+++ src/vm/compat_shims.h
@@ -1,3 +1,4 @@
+// Compatibility shims; ideally, this entire file will be gone at some point.
 #ifndef COY_COMPAT_SHIMS_H_
 #define COY_COMPAT_SHIMS_H_
 
@@ -13,11 +14,12 @@
 #include <stdint.h>
 #include "../util/bitarray.h"
 
-// compatibility shims
+// these are internal, which is why they end with '_' and had *no* typedef
 typedef union coy_instruction_ coy_instruction_t;
 typedef struct coy_function_block_ coy_function_block_t;
 typedef struct coy_function_ coy_function_t;
 
+// temporary APIs
 void coy_push_uint(coy_context_t* ctx, uint32_t u);
 void coy_vm_exec_frame_(coy_context_t* ctx);
 
